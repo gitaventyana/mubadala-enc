021b79c2103a27e144709b8040e7578accc00391
Gita Ventyana <gita.ventyana@gmail.com>
2024-06-28 13:47:55 +0700

    header menu

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const menuBtn = document.getElementById("header-menu-btn");
+const menu = document.querySelector("nav");
+
+menuBtn.onmouseenter = hoverMenu;
+menuBtn.onmouseleave = mouseleaveMenu;
+menuBtn.onclick = clickMenu;
+menu.onmouseenter = hoverMenu;
+menu.onmouseleave = mouseleaveMenu;
+
+function hideMenu() {
+  menu.className = "hide";
+}
+function hoverMenu() {
+  menu.classList.remove("hide");
+}
+function mouseleaveMenu() {
+  if (!menu.classList.contains("clicked")) {
+    hideMenu();
+  }
+}
+function clickMenu() {
+  if (menu.classList.contains("hide")) {
+    menu.className = "clicked";
+  } else {
+    hideMenu();
+  }
+}
+
 const docBtn = document.getElementById("res-doc-btn");
 const vidBtn = document.getElementById("res-vid-btn");
 docBtn.onclick = resourceTabClick;
