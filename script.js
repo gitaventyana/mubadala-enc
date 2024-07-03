b907cd467cd4de751a4655ed69fca49ffbe3da64
Gita Ventyana <gita.ventyana@gmail.com>
2024-07-04 00:49:22 +0700

    slider draft

--- script.js
+++ script.js
@@ -40,6 +40,66 @@ function toggleMute() {
   }
 }
 
+//
+// Training section
+//
+
+// Make the DIV element draggable:
+dragElement(document.getElementById("date-pin"));
+
+function dragElement(elmnt) {
+  let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  const slider = document.querySelector(".slider-container");
+  elmnt.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    if (
+      elmnt.offsetLeft - pos1 >= 0 &&
+      elmnt.offsetLeft - pos1 < slider.offsetWidth - elmnt.offsetWidth
+    ) {
+      let newPos = elmnt.offsetLeft - pos1;
+      elmnt.style.left = newPos + "px";
+    }
+  }
+
+  function closeDragElement() {
+    // snap pin
+    let rounded = Math.round(elmnt.offsetLeft / (slider.offsetWidth / 12));
+    let newPos = (slider.offsetWidth / 12) * rounded;
+    elmnt.style.left = newPos + "px";
+
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+
+    // fetch current month's events
+  }
+}
+
+//
+// Resources section
+//
+
 const docBtn = document.getElementById("res-doc-btn");
 const vidBtn = document.getElementById("res-vid-btn");
 docBtn.onclick = resourceTabClick;
